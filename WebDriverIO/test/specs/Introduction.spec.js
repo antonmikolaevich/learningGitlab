1450e81b7a6bcc067908650ee03be5086acb5679
Anastasia Blinova <Anastasia_Blinova@epam.com>
2023-09-12 20:45:34 +0300

    Introduction

--- WebDriverIO/test/specs/Introduction.spec.js
+++ WebDriverIO/test/specs/Introduction.spec.js
@@ -3,23 +3,48 @@ describe("Introduction",()=>{
         await browser.url('https://www.epam.com/')
       });
 
-      it ("Find 'Services' button on the page'", async()=>{
-        await $('//*[@href="/services"]');
+      it ("Click 'Services' button on the page'", async()=>{
+         const buttonServices = $('button.top-navigation__item-link--a11y');
+         buttonServices.scrollIntoView();
+         buttonServices.click();
+         const pageTitle = await browser.getTitle();
+ 
+         expect(pageTitle).toEqual("EPAM | Software Engineering & Product Development Services");
+      } );
+
+
+     it ("Click 'Industries' button on the page'", async()=>{
+        const buttonIndustries = await $('//*[@href="/insights"]');
+        buttonIndustries.scrollIntoView();
+        buttonIndustries.click();
+        const pageTitle = await browser.getTitle();
+        
+        expect(pageTitle).toEqual("EPAM | Software Engineering & Product Development Services");
      } );
 
 
-     it ("Find 'Industries' button on the page'", async()=>{
-        await $('//*[@href="/insights"]');
+     it ("Open 'BLOG' page'", async()=>{
+        const blogelement = await $('//*[@href="https://www.epam.com/insights/blogs/mastering-cloud-costs"]');
+        blogelement.scrollIntoView();
+        blogelement.click();
+        const wholepage = $('div.scaling-of-text-wrapper');
+        await expect( wholepage).toHaveText('Mastering Cloud Costs');
      } );
 
+     it ("Click 'Global' button on the page'", async()=>{
+        const globalButton = await $('//*[@class="location-selector__button-language"]');
+        globalButton.scrollIntoView();
+        globalButton.click();
 
-     it ("Find 'Switcher' radio-button on the page'", async()=>{
-        await $('//*[@class="switch"]');
-     } );
+        const panel = await $('nav.location-selector__panel');
+        await panel.waitForExist({
+         timeout: 5000,
+         revert: true,
+         timeoutMsg: "Sorry, the element is not displayd.",
+         interval: 50
+        });
+        expect(await panel.isExisting()).toEqual(true);
 
-
-     it ("Find 'Global' button on the page'", async()=>{
-        await $('//*[@class="location-selector__button-language"]');
      } );
 
     })

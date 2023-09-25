92cf5b6b9b7b5f2c420755cc69a86961e59a910c
Anastasia Blinova <Anastasia_Blinova@epam.com>
2023-09-25 18:56:21 +0300

    get

--- API/test.spec.js
+++ API/test.spec.js
@@ -1,12 +1,14 @@
 const axios = require("axios");
 
 describe("API Test Suite", () => {
-    it("GET post/1", async()=> {
+    it("GET", async()=> {
         const response = await axios({
-            url: "https://jsonplaceholder.typicode.com/posts/1",
+            url: "https://www.epam.com/content/epam/en/_jcr_content/contexthub.pagedata.json",
             method: "get",
         })
-        expect(response.status).to.equal(200)
-        expect(response.data.userId).to.equal(1)
+ //       console.log(response.data);
+  expect(response.status).to.equal(200)
+  expect(response.data.title).to.equal('EPAM | Software Engineering & Product Development Services  ')
+
     })
 })

dd0f664765b4b5369dd4e83b5573ac654e4be2ec
Anastasia Blinova <Anastasia_Blinova@epam.com>
2023-09-26 21:03:24 +0300

    get put post delete

--- API/specs/test.spec.js
+++ API/specs/test.spec.js
@@ -3,12 +3,20 @@ const {testDataPost} = require("./../config/datapost.json");
 const {testDataPut} = require("./../config/dataput.json")
 
 describe("API Test Suite", () => {
-    it("GET", async()=> {
-        const response = await sendRequest ("posts/1");
+    it("GET - List all resources", async () => {
+        const response = await sendRequest("posts");
+        expect(response.status).to.equal(200);
+    });
 
+    it("GET - get a resource", async()=> {
+        const response = await sendRequest ("posts/1");
         expect(response.status).to.equal(200)
         expect(response.data.userId).to.equal(1)
+    });
 
+    it("GET - Filter resources", async () => {
+        const response = await sendRequest("posts?userId=1");
+        expect(response.status).to.equal(200);
     });
 
     it ("POST", async()=>{
